b3a598f9bfd41b463cf3addf9103a332ac1c53dd
Paras-C <paras_c@hotmail.co.uk>
2017-10-23 13:48:14 +0100

    DB VM created and provisioned

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -2,3 +2,17 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo locale-gen en_GB.UTF-8
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo cp /home/ubuntu/environment/db/mongodb.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+sudo rm /etc/mongod.conf
+sudo cp /home/ubuntu/environment/db/mongod.conf /etc
+
+sudo service mongod restart
